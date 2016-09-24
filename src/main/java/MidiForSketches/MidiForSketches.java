3f09554bf2b06c5404d96015e0c5a073f17f535e
James McElwain <jmcelwain@gmail.com>
2016-09-24 13:37:51 -0500

    indentation

--- src/main/java/MidiForSketches/MidiForSketches.java
+++ src/main/java/MidiForSketches/MidiForSketches.java
@@ -8,31 +8,31 @@ import java.util.HashMap;
  *  input.
  */
 public class MidiForSketches {
-   private String[] deviceNames;
-   private HashMap<String, MidiDeviceContainer> midiDevices = new HashMap<>();
+    private String[] deviceNames;
+    private HashMap<String, MidiDeviceContainer> midiDevices = new HashMap<>();
 
-   public MidiForSketches(String... devices) {
-      for (String device : devices) {
-         this.deviceNames = devices;
-         midiDevices.put(device, new MidiDeviceContainer(device));
-      }
-   }
+    public MidiForSketches(String... devices) {
+        for (String device : devices) {
+            this.deviceNames = devices;
+            midiDevices.put(device, new MidiDeviceContainer(device));
+        }
+    }
 
-   public void refreshDevice(String name) {
-      this.midiDevices.get(name).connect();
-   }
+    public void refreshDevice(String name) {
+        this.midiDevices.get(name).connect();
+    }
 
-   public void refreshAll() {
-      for (String device : deviceNames) {
-         refreshDevice(device);
-      }
-   }
+    public void refreshAll() {
+        for (String device : deviceNames) {
+            refreshDevice(device);
+        }
+    }
 
-   public void setDevice(String name) {
-      this.midiDevices.put(name, new MidiDeviceContainer(name));
-   }
+    public void setDevice(String name) {
+        this.midiDevices.put(name, new MidiDeviceContainer(name));
+    }
 
-   public MidiDeviceContainer getDevice(String name) {
-      return this.midiDevices.get(name);
-   }
+    public MidiDeviceContainer getDevice(String name) {
+        return this.midiDevices.get(name);
+    }
 }
