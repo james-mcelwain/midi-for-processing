5e6b85dcbea9c6b367e76efeb5fa963ccfe904d8
James McElwain <jmcelwain@gmail.com>
2016-09-24 13:35:30 -0500

    more public interface

--- src/main/java/MidiForSketches/MidiForSketches.java
+++ src/main/java/MidiForSketches/MidiForSketches.java
@@ -6,13 +6,33 @@ import java.util.HashMap;
  *  Midi plugin for Processing, to enable the use of
  *  variables whose values are controlled by external midi
  *  input.
-        */
+ */
 public class MidiForSketches {
+   private String[] deviceNames;
    private HashMap<String, MidiDeviceContainer> midiDevices = new HashMap<>();
 
    public MidiForSketches(String... devices) {
       for (String device : devices) {
+         this.deviceNames = devices;
          midiDevices.put(device, new MidiDeviceContainer(device));
       }
+   }
+
+   public void refreshDevice(String name) {
+      this.midiDevices.get(name).connect();
+   }
+
+   public void refreshAll() {
+      for (String device : deviceNames) {
+         refreshDevice(device);
+      }
+   }
+
+   public void setDevice(String name) {
+      this.midiDevices.put(name, new MidiDeviceContainer(name));
+   }
+
+   public MidiDeviceContainer getDevice(String name) {
+      return this.midiDevices.get(name);
    }
 }
